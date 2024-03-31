6784ac9475aa36956cbbd57d7a9515f8b7bb4ccf
Mr-3b0y <mr_3b0y@protonmail.com>
2024-03-31 14:34:53 -0400

    fix create_admin

--- usuarios/management/commands/create_admin.py
+++ usuarios/management/commands/create_admin.py
@@ -8,7 +8,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if os.getenv("SU_USER") and os.getenv("SU_PASSWORD"):
-            Usuario.objects.create_superuser(username=os.getenv("SU_USER"), password=os.getenv("SU_PASSWORD"))
+            user = Usuario.objects.get(username=os.getenv("SU_USER"))
+            if not user:
+                Usuario.objects.create_superuser(username=os.getenv("SU_USER"), password=os.getenv("SU_PASSWORD"))
             self.stdout.write(self.style.SUCCESS('Administrador creado exitosamente'))
         else:
             self.stdout.write(self.style.ERROR_OUTPUT('No se configuraron las variables de entorno SU_USER y SU_PASSWORD'))
